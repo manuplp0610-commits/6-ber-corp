72ea07eb89f6a19b846e7e8bf7fda72ec2d26f8d
Manwel Nova <manwel.nova@gmail.com>
2026-09-02 19:11:21 +0200

    create nav mobile

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,12 +1,33 @@
-import { Link } from "react-router-dom";
-
 import logo from "../../assets/images/logo.png";
-
 import "./navBar.css";
+import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function NavBar({ variant = "default" }) {
+  const location = useLocation();
+
+  const [openNav, setOpenNav] = useState(false);
+
+  const handleClick = () => {
+    setOpenNav((prev) => !prev);
+  };
+
+  useEffect(() => {
+    setOpenNav(false);
+  }, [location.pathname]);
+
   return (
     <nav className={`navBar navBar--${variant}`}>
+      {/* Bouton mobile */}
+      <button
+        onClick={handleClick}
+        className="nav-toggle"
+        aria-label={openNav ? "Fermer le menu" : "Ouvrir le menu"}
+        aria-expanded={openNav}
+        aria-controls="nav-mobile"
+      >
+        ☰
+      </button>
       {/* Logo gauche - desktop */}
       <Link to="/" className="logo-link logo-link--left">
         <img
@@ -43,39 +64,33 @@ export default function NavBar({ variant = "default" }) {
         </div>
       </div>
 
-      {/* Bouton mobile */}
-      <button
-        className="nav-toggle"
-        aria-label="Ouvrir le menu"
-        aria-expanded="false"
-        aria-controls="nav-mobile"
-      >
-        ☰
-      </button>
-
       {/* Menu mobile */}
-      <div className="nav-mobile" id="nav-mobile">
-        <ul>
-          <li>
-            <Link to="/shop">Boutique</Link>
-          </li>
-          <li>
-            <Link to="/bar">Bar</Link>
-          </li>
-          <li>
-            <Link to="/console">Consoles</Link>
-          </li>
-          <li>
-            <Link to="/computer">PC</Link>
-          </li>
-          <li>
-            <Link to="/event">Événements</Link>
-          </li>
-          <li>
-            <a href="#footer">Contact</a>
-          </li>
-        </ul>
-      </div>
+      {openNav && (
+        <div className="nav-mobile" id="nav-mobile">
+          <div className="nav-mobile-content">
+            <ul>
+              <li>
+                <Link to="/shop">Boutique</Link>
+              </li>
+              <li>
+                <Link to="/bar">Bar</Link>
+              </li>
+              <li>
+                <Link to="/console">Consoles</Link>
+              </li>
+              <li>
+                <Link to="/computer">PC</Link>
+              </li>
+              <li>
+                <Link to="/event">Événements</Link>
+              </li>
+              <li>
+                <a href="#footer">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
